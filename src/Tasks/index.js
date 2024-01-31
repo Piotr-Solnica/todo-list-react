6a176023aa17ffda655fb71a1629f8f18f060282
Piotr Solnica <piotr.solnica.ps@gmail.com>
2024-01-31 22:58:19 +0100

    structure improved

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,19 +1,21 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideTasksDone}) => (
+const Tasks = ({ tasks, hideTasksDone }) => (
   <ul className="tasksList">
-    {tasks.map(task =>(
+    {tasks.map(task => (
       <li key={task.id} className={`tasksList__item ${task.done && hideTasksDone ? "tasksList__item--hidden" : ""}`}>
-         <button className="tasksList__button tasksList__button--done">
-              {task.done ? "✔" : ""}
-            </button>
-            <span className={`tasksList__content ${task.done ? "tasksList__content--done" : ""}`}>
-             {task.content} 
-             </span>
-            <button className="tasksList__button tasksList__button--remove">
-             🗑
-            </button>  
-      </li>      
+        <button className="tasksList__button tasksList__button--done">
+          {task.done ? "✔" : ""}
+        </button>
+        <span className={`tasksList__content ${task.done ? "tasksList__content--done" : ""}`}>
+          {task.content}
+        </span>
+        <button
+          className="tasksList__button tasksList__button--remove"
+        >
+          🗑
+        </button>
+      </li>
     ))}
   </ul>
 );
